eb4dfd97fb571f25950c65d84625d59597b25d4a
bolivarescobar <62059442+bolivarescobar@users.noreply.github.com>
2020-03-19 20:22:36 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,26 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var myVar = [3, "Jojoca", true, null, undefined]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+var arr = [1, 2, 3];
+
+function myFunction(a) {
+    return a;
+}
+
+myFunction(arr);
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(myFunction(arr)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
