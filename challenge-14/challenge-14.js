189b55182c8282993bf05d5f84769e632b4c36d2
bolivarescobar <62059442+bolivarescobar@users.noreply.github.com>
2020-06-10 14:26:15 -0300

    Update challenge-14.js

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -111,13 +111,19 @@
     e atribuirá o seu nome invertido (usando o array criado acima).
     */
     console.log( '\nInversed Name:' );
-    // ?
+    
+    var inversedName = name.reduceRight(function(acumulado, atual) { 
+        return acumulado + atual;
+    });
+
+    console.log( inversedName );
 
     /*
     Mostre no console o array `numberObjects`.
     */
     console.log( '\nNumber objects' );
-    // ?
+    
+    console.log( numberObjects );
 
     /*
     Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -129,20 +135,44 @@
     o que acontece ;)
     */
     console.log( '\nExiste um { number: 2 } em numberObjects?' );
-    // ?
+
+    var obj = numberObjects[1];
+    
+    if ( numberObjects.indexOf( obj ) > -1 ) {
+
+        console.log( "Existe um objeto { number: 2 } em numberObjects!")
+
+    } else { 
+
+    console.log( "Não existe um objeto { number: 2 } em numberObjects :(" )
+
+    };
 
     /*
     Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
     será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
     */
     console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-    // ?
+    
+    if ( numberObjects.lastIndexOf( obj ), 2 > -1 ) {
+
+        console.log( "Existe um objeto { number: 2 } em numberObjects!")
+
+    } else { 
+
+    console.log( "Não existe um objeto { number: 2 } em numberObjects :(" )
+
+    };
 
     /*
     Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
     formato de String.
     */
     console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-    // ?
+    
+    if ( Array.isArray( justMod2Or3 ) ) {
+        console.log( justMod2Or3.toString ); };
+
+    
 
 })();
